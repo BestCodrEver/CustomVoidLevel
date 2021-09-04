d8165345417caa9f2f958eb2ebda6b15e442562e
BestCodrEver <70841501+BestCodrEver@users.noreply.github.com>
2021-09-04 10:36:12 +1000

    Update Main.php

--- src/BestCodrEver/CustomVoidLevel/Main.php
+++ src/BestCodrEver/CustomVoidLevel/Main.php
@@ -5,8 +5,26 @@ namespace BestCodrEver\CustomVoidLevel;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\{player\PlayerMoveEvent, Listener};
 use pocketmine\utils\{TextFormat as TF, Config};
+use pocketmine\{Player, Server};
 
 class Main extends PluginBase implements Listener
 {
+  //Config File
+  public $config;
   
+  public function onEnable(): void
+  {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    //Save Config
+    $this->saveResource("config.yml");
+    $this->config = $plugin->getConfig();
+  }
+  
+  public function onMove(PlayerMoveEvent $event): void
+  {
+    $player = $event->getPlayer();
+    //Get the player's Y-Level to check if it's in the void level
+    $playerY = $player->getY();
+    if ($playerY !== $config->get("void-y-level")) return;
+  }
 }
