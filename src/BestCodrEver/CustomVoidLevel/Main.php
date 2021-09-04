ffab32463cb1eee38f4b3b9d6a13f1ce9ca2eeab
BestCodrEver <70841501+BestCodrEver@users.noreply.github.com>
2021-09-04 10:18:13 +1000

    Update Main.php

--- src/BestCodrEver/CustomVoidLevel/Main.php
+++ src/BestCodrEver/CustomVoidLevel/Main.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace BestCodrEver\CustomVoidLevel;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\{player\PlayerMoveEvent, Listener};
+use pocketmine\utils\{TextFormat as TF, Config};
+
+class Main extends PluginBase implements Listener
+{
+  
+}
